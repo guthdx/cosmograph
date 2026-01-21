812d3a1e482e1fe538e7b857f7c425ccc0859f7a
Guthrie Ducheneaux <guthdx@gmail.com>
2026-01-21 12:27:42 -0700

    feat(02-01): export PdfExtractor from extractors module
    - Add PdfExtractor import to extractors __init__.py
    - Add PdfExtractor to __all__ list
    - Maintain alphabetical ordering of imports and exports

--- src/cosmograph/extractors/__init__.py
+++ src/cosmograph/extractors/__init__.py
@@ -1,8 +1,15 @@
 """Document extractors for building knowledge graphs."""
 
 from .base import BaseExtractor
-from .text import TextExtractor
-from .legal import LegalDocumentExtractor
 from .generic import GenericExtractor
+from .legal import LegalDocumentExtractor
+from .pdf import PdfExtractor
+from .text import TextExtractor
 
-__all__ = ["BaseExtractor", "TextExtractor", "LegalDocumentExtractor", "GenericExtractor"]
+__all__ = [
+    "BaseExtractor",
+    "GenericExtractor",
+    "LegalDocumentExtractor",
+    "PdfExtractor",
+    "TextExtractor",
+]
